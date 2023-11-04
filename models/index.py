7c1d53564644494b8bb3896e3109c091e94cd2b2
Thi Phan <thiphan94@gmail.com>
2023-11-04 16:48:47 +0100

    models

---
+++ models/index.py
@@ -0,0 +1,4 @@
+from conn.db import engine, meta
+from models.user import users
+
+meta.create_all(engine)
---
+++ models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Table, Column, Integer, String
+from conn.db import meta
+
+# from sqlalchemy.sql.sqltypes import Integer, String
+
+users = Table(
+    "users",
+    meta,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("name", String(50)),
+    Column("email", String(50)),
+    Column("password", String(50)),
+)
